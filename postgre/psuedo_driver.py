cdd6537a36d2c9c0d415c7b7ab86e1ddae6bdd5c
huntmckay <huntmckay@gmail.com>
2023-06-06 07:06:59 -0400

    clean up postgre files

---
+++ postgre/psuedo_driver.py
@@ -0,0 +1,45 @@
+import *
+
+class dbmanger:
+
+    def __init__():
+        
+        self.connection = connection
+        self.creds      = creds
+
+    def create():
+
+        '''
+        pytest thingy
+        '''
+
+        # Create tables
+
+        # Create column
+
+        # Create Row
+
+    def read():
+
+        # Read entire table w/args
+
+        # Read column w/args
+
+        # Read row w/args
+        
+        def __repr__():
+
+            return nice_value
+
+
+    def update():
+        
+        # Update record
+
+    def delete():
+
+        # Delete Table
+
+        # Delete Column
+
+        # Delete Record
